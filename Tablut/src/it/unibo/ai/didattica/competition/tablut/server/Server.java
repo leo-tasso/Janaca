0535b63e3ac8a04725af01f3b89d3dcd91f4622f
Leonardo Tassinari <tassinari.leonardo.2000@gmail.com>
2024-11-19 00:48:53 +0100

    add unlimited time in server

--- Tablut/src/it/unibo/ai/didattica/competition/tablut/server/Server.java
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/server/Server.java
@@ -118,7 +118,7 @@ public class Server implements Runnable {
 	 * 
 	 */
 	public static void main(String[] args) {
-		int time = 20;
+		int time = 55555560;
 		int moveCache = -1;
 		int repeated = 0;
 		int errors = 0;
